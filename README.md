569d07ba12bf30db8bec62f136abb83e66c4d06e
syzwina <nor.syazwina.ahmad.fazrul@gmail.com>
2021-11-15 09:31:01 +0800

    added some necessary files and basic navigation layout

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # responsive-web-page
 I will try the grid approach for this website.
+
+resources
+1. https://www.w3schools.com/css/tryit.asp?filename=tryresponsive_col-s
---
+++ main.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var x = document.getElementById("mySOCIALS");
+    var y = document.getElementById("myNAV-PAGES");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
